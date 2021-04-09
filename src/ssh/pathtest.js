4c9f4565ddfec69615087f7f3d9f0352d34927ba
张博文 <18211132604@163.com>
2021-04-09 17:25:02 +0800

    update

--- src/ssh/pathtest.js
+++ src/ssh/pathtest.js
@@ -1,6 +1,7 @@
 const path  = require('path')
 const os  = require('os')
 console.log("Bowen: os.cpus", os.cpus())
+console.log("Bowen: os.cpus", os.type())
 console.log('__dirname',__dirname);
 console.log('__filename',__filename);
 console.log('test',path.resolve('/test/my-node2'))
