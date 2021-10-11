76279d0431aa6eef6217a5c8f350f0eab58202fe
张博文 <18211132604@163.com>
2021-10-11 15:03:38 +0800

    Update 添加批量上传博客园脚本

---
+++ src/blog/api.js
@@ -0,0 +1,115 @@
+var parser = require("xml2json");
+var axios = require("axios");
+
+const _axios = axios.create();
+
+const common = `
+<param>
+<value><string>bitbw</string></value>
+</param>
+<param>
+<value><string>bowen591005606</string></value>
+</param>
+`;
+
+// 添加响应拦截器
+axios.interceptors.response.use(
+  (response) => {
+    // 对响应数据做点什么
+    const json = parser.toJson(response.data);
+    const data = JSON.parse(json);
+    return data;
+  },
+  (error) => {
+    // 对响应错误做点什么
+    let msg = error.response && error.response.data.message ? error.response.data.message : error;
+    console.error(error.response ? `${error.response.status} : ${msg}` : `网络不可用`);
+    return Promise.reject(error);
+  }
+);
+// 发布或修改文章
+function pushPost({ type = "add", content, title, postid }) {
+  return axios.post(
+    "https://rpc.cnblogs.com/metaweblog/bitbw",
+    `<?xml version="1.0"?>
+    <methodCall>
+    <methodName>metaWeblog.${type === "add" ? "newPost" : "editPost"}</methodName>
+    <params>
+    <param>
+        <value><string>${type === "add" ? "623687" : postid}</string></value>
+    </param>
+    ${common}
+        <param>
+            <value>
+                    <struct>
+                        <member>
+                            <name>description</name>
+                            <value>
+                                <string><![CDATA[${content}]]></string>
+                            </value>
+                        </member>
+                        <member>
+                            <name>title</name>
+                            <value>
+                                <string><![CDATA[${title}]]></string>
+                            </value>
+                        </member>
+                        <member>
+                            <name>categories</name>
+                            <value>
+                                <array>
+                                    <data>
+                                        <value>
+                                            <string>[Markdown]</string>
+                                        </value>
+                                    </data>
+                                </array>
+                            </value>
+                        </member>
+                    </struct>
+                </value>
+        </param>
+        <param>
+            <value><boolean>1</boolean></value>
+        </param>
+    </params>
+    </methodCall>`
+  );
+}
+// 获取文章数据
+function getPost({ postid }) {
+  return axios.post(
+    "https://rpc.cnblogs.com/metaweblog/bitbw",
+    `<?xml version="1.0"?>
+<methodCall>
+ <methodName>metaWeblog.getPost</methodName>
+  <params>
+    <param>
+        <value><string>${postid}</string></value>
+    </param>
+   ${common}
+  </params>
+</methodCall>`
+  );
+}
+
+// var xml = "<foo attr=\"value\">bar</foo>";
+// console.log("input -> %s", xml)
+
+// // xml to json
+// var json = parser.toJson(xml);
+// console.log("to json -> %s", json);
+
+// // json to xml
+// var xml = parser.toXml(json);
+// console.log("back to xml -> %s", xml)
+
+// blogid 623687 url https://www.cnblogs.com/bitbw/ blogName _Bowen
+// getPost(15392207).then(res=>{
+//   console.log("Bowen: res", res)
+
+//   })
+module.exports = {
+  pushPost,
+  getPost,
+};
---
+++ src/blog/postBlog.js
@@ -0,0 +1,96 @@
+const fs = require("fs").promises;
+const path = require("path");
+const YAML = require("yaml");
+const crypto = require("crypto");
+
+const { pushPost, getPost } = require("./api");
+
+const dirPath = "C:/E盘资料/my-blog/my-blog/source/_posts";
+
+// 发布所有的文章
+async function hanleAllPushPost() {
+  let files = await fs.readdir(dirPath);
+  for (const fileName of files) {
+    if (!/\.md/.test(fileName)) continue;
+    console.log("[********************************]");
+    console.log("[fileName]", fileName);
+    const filePath = path.resolve(dirPath, fileName);
+    // await new Promise((r) => setTimeout(r, 1000), true);
+    await handlePushPost(filePath);
+  }
+}
+// 将文章原始字符分解为obj数据
+function genArticleDate(articleOrigin) {
+  const datas = articleOrigin.split("---");
+  const temp = datas[1].replace(/\t/g, "");
+  const titleObj = YAML.parse(temp);
+  const contentData = datas[2];
+  return {
+    titleObj,
+    contentData,
+  };
+}
+// 将obj数据合成文章
+function genArticleStr({ titleObj, contentData }) {
+  const titleStr = "\n" + YAML.stringify(titleObj);
+  const datas = ["", titleStr, contentData];
+  const str = datas.join("---");
+  return str;
+}
+// 根据path修改或者新建文章
+async function handlePushPost(filePath) {
+  const fileName = path.basename(filePath);
+  const articleOrigin = await fs.readFile(filePath, "utf-8");
+  const { titleObj, contentData } = genArticleDate(articleOrigin);
+  const hash = crypto.createHash("sha256");
+  hash.update(contentData);
+  // 当前hash
+  let nowContentHash = hash.digest("hex");
+  let { cnblogs, hash: contentHash } = titleObj;
+  let res;
+  if (contentHash && contentHash == nowContentHash) {
+    console.log("[hash值未变退出当前循环]");
+    return;
+  }
+  // yaml中添加 hash
+  titleObj.hash = nowContentHash;
+  // 编辑
+  if (cnblogs && cnblogs.postid) {
+    console.log("[-------------修改-------------]");
+    res = await pushPost({
+      type: "edit",
+      content: contentData,
+      title: titleObj.title,
+      postid: cnblogs.postid,
+    });
+    if (res.methodResponse.fault) {
+      console.log("[修改失败]", res.methodResponse.fault.value.struct.member[1].value.string);
+      throw Error(res.methodResponse.fault.value.struct.member[1].value.string);
+    }
+    console.log("[修改成功]", res.methodResponse.params.param.value.boolean);
+  } else {
+    console.log("[-------------新建-------------]");
+    titleObj.cnblogs = {};
+    res = await pushPost({
+      type: "add",
+      content: contentData,
+      title: titleObj.title,
+    });
+    res;
+    if (res.methodResponse.fault) {
+      console.log("[上传失败]", res.methodResponse.fault.value.struct.member[1].value.string);
+      throw Error(res.methodResponse.fault.value.struct.member[1].value.string);
+    }
+    console.log("[上传成功]", res.methodResponse.params.param.value.string);
+    // yaml中添加 postid
+    titleObj.cnblogs.postid = res.methodResponse.params.param.value.string;
+  }
+  // 回写数据
+  const str = genArticleStr({ titleObj, contentData });
+  await fs.writeFile(filePath, str);
+  console.log("[回写成功]", fileName);
+}
+
+// test
+// handlePushPost("C:/E盘资料/my-blog/my-blog/source/_posts/js中的微观任务和宏观任务.md");
+hanleAllPushPost();
