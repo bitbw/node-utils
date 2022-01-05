5a8176372396d9d716e1e3b561bad3405aa13b7b
张博文 <18211132604@163.com>
2022-01-05 14:05:33 +0800

    update

--- src/crypto/cryptoJs.mjs
+++ src/crypto/cryptoJs.mjs
@@ -17,3 +17,5 @@ var ciphertext = CryptoJS.AES.encrypt("my message", "secret key 123").toString()
 var bytes = CryptoJS.AES.decrypt(ciphertext, "secret key 123");
 var originalText = bytes.toString(CryptoJS.enc.Utf8);
 console.log(originalText); // 'my message'
+
+
